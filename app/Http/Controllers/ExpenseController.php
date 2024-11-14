2e6b59d84f16878366b3ae4f52f68f3e4f0b3fd8
LENOVO <michaelfernandezdev@gmail.com>
2024-11-14 13:10:08 -0600

    first commit

---
+++ app/Http/Controllers/ExpenseController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExpenseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index():JsonResponse
+    {
+        $exponses=Expense::latest()->get();
+        return response()->json($expenses, Response::HTTP_OK);
+
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(SaveExpenseRequest $request):JsonResponse
+    {
+        $expense=Expense::create($request->validated());
+        return response()->json($expense, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id):JsonResponse
+    {
+        $expense=Expense::findOrFail($id);
+        return response()->json($expense, Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(SaveExpenseRequest $request, string $id):JsonResponse
+    {
+        $expense=Expense::findOrFail($id);
+        $expense->update($request->validated());
+        return response()->json($expense, Response::HTTP_OK);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $expense=Expense::findOrFail($id);
+        $expense->delete();
+        return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ app/Models/Expense.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    use HasFactory;
+    protected $fillable = ['type', 'amount', 'description','expense_date'];
+}
